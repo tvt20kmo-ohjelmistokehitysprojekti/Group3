1aa236ace43fc419b76bec522a5810ad891943b5
Joonas Kinnunen <joonas.kinn@gmail.com>
2020-11-07 14:25:26 +0200

    update gitignore

--- atm-backend-nodejs/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./App')
-const http = require('http')
-
-const server = http.createServer(app)
-
-const PORT = 3001
-server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
